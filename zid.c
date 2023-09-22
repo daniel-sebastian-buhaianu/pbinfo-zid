2f0b62d907253be3fd9a06456988c40ed84eebf0
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-09-22 18:45:00 +0300

    Update zid.c

--- zid.c
+++ zid.c
@@ -8,17 +8,18 @@
 #define MIN_CMAX 1
 #define MAX_CMAX 9
 
+int min(int a, int b);
+
 int main()
 {
-	unsigned short n, m, cmax, ***fr, **a, i, j, cif, c, L, LMax, imax, jmax, frec, fru, ii, jj;
-	char ok;
-
 	FILE *fin = fopen("zid.in", "r");
 
 	if (!fin) {
 		printf("Eroare fisier zid.in\n");
 		return 1;
 	}
+
+	unsigned short n, m, cmax;
 
 	fscanf(fin, "%hu %hu %hu", &n, &m, &cmax);
 
@@ -37,50 +38,86 @@ int main()
 		return 4;
 	}
 
+	unsigned short ***fr, i, **a, c, j;
+
 	fr = (unsigned short***)calloc(n+1, sizeof(unsigned short**));
+
+	if (!fr) {
+		printf("Eroare alocare memorie *fr\n");
+		return 5;
+	}
 
 	for (i = 0; i <= n; i++) {
 		fr[i] = (unsigned short**)calloc(m+1, sizeof(unsigned short*));
 
+		if (!fr[i]) {
+			printf("Eroare alocare memorie *fr\n");
+			return 5;
+		}
+
 		for (j = 0; j <= m; j++) {
-			fr[i][j] = (unsigned short*)calloc(cmax+1, sizeof(unsigned short));
+			fr[i][j] = (unsigned short*)calloc(cmax+1, sizeof(unsigned short)); 
+			
+			if (!fr[i][j]) {
+				printf("Eroare alocare memorie *fr\n");
+				return 5;
+			}
 		}
 	}
 
 	a = (unsigned short**)calloc(n+1, sizeof(unsigned short*));
 
+	if (!a) {
+		printf("Eroare alocare memorie *a\n");
+		return 6;
+	}
+
 	for (i = 1; i <= n; i++) {
 		a[i] = (unsigned short*)calloc(m+1, sizeof(unsigned short));
 
+		if (!a[i]) {
+			printf("Eroare alocare memorie *a\n");
+			return 6;
+		}
+
 		for (j = 1; j <= m; j++) {
-			fscanf(fin, "%hu", &cif);
-			
-			a[i][j] = cif;
+			fscanf(fin, "%hu", &a[i][j]);
 
 			for (c = MIN_CMAX; c <= cmax; c++)
 				fr[i][j][c] = fr[i][j-1][c] + fr[i-1][j][c] - fr[i-1][j-1][c];
 
-			fr[i][j][cif] += 1;
+			fr[i][j][a[i][j]] += 1;
 		}
+	}
+
+	unsigned short LMax, jmax, imax, L, ii, jj, *frec, cate;
+
+	frec = (unsigned short*)calloc(cmax+1, sizeof(unsigned short));
+
+	if (!frec) {
+		printf("Eroare alocare memorie *frec\n");
+		return 7;
 	}
 
 	for (LMax = jmax = imax = i = 1; i <= n; i++) {
 		for (j = 1; j <= m; j++) {
-			for (L = n-i+1; L >= 1 && L > LMax && j+L-1 <= m; L--) {
+			for (L = min(n-i+1, m-j+1); L > LMax; L--) {
 				ii = i+L-1, jj = j+L-1;
 
-				for (ok = 1, fru = 0, c = MIN_CMAX; c <= cmax && ok; c++) {
-					frec = fr[ii][jj][c] - fr[ii][jj-L][c] - fr[i-1][jj][c] + fr[i-1][j-1][c];
+				for (c = MIN_CMAX; c <= cmax; c++)
+					frec[c] = fr[ii][jj][c] - fr[ii][j-1][c] - fr[i-1][jj][c] + fr[i-1][j-1][c];
 
-					if (frec && !fru)
-						fru = frec;
+				for (c = MIN_CMAX; c < cmax && !frec[c]; c++);
 
-					if (frec && fru && frec != fru)
-						ok = 0;	
-				}
+				cate = frec[c];
 
-				if (ok && L > LMax)
-					LMax = L, imax = i, jmax = j;
+				for (; c <= cmax; c++)
+					if (frec[c] && frec[c] != cate)
+						break;
+
+				if (c == cmax+1)
+					if (L > LMax)
+						LMax = L, imax = i, jmax = j;
 			}
 		}
 	}
@@ -102,7 +139,13 @@ int main()
 
 	free(fr);
 	free(a);
+	free(frec);
 
 	return 0;
 }
-// scor 49
+
+int min(int a, int b)
+{
+	return a < b ? a : b;
+}
+// scor 85
